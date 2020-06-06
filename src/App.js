2c90f2c36ee834cb9946c55fb028ad6e81d0f8dd
Levon Kirakosyan <noosx.e@gmail.com>
2020-06-06 12:44:42 -0700

    Add image upload and canvas preview support.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const fileInputEl = React.useRef(null);
+  const canvasEl = React.useRef(null);
+
+  const draw = (event) => {
+    const { path } = event;
+
+    if (!path || path.length === 0) {
+      return;
+    }
+
+    const canvas = canvasEl.current;
+
+    if (canvas === null) {
+      return;
+    }
+
+    const image = path[0];
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+  };
+
+  const handleChange = () => {
+    const fileInput = fileInputEl.current;
+
+    if (fileInput === null) {
+      return;
+    }
+
+    const files = fileInput.files;
+
+    if (!files || files.length === 0) {
+      return;
+    }
+
+    var img = new Image();
+    img.addEventListener("load", draw);
+    img.onerror = (err) => {
+      console.error(err);
+    };
+    img.src = URL.createObjectURL(files[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputEl}
+        onInput={handleChange}
+      />
+      <canvas ref={canvasEl} />
     </div>
   );
 }
