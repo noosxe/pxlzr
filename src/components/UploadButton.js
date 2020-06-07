d8fc3266b2cd14fd63f248e6f79594b016c22635
Levon Kirakosyan <noosx.e@gmail.com>
2020-06-06 21:44:56 -0700

    Add an icon to upload button.

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 const useStyles = makeStyles((theme) => ({
   input: {
@@ -40,7 +41,12 @@ function UploadButton({ onChange = () => {} }) {
         onInput={handleChange}
       />
       <label htmlFor="file-upload-button">
-        <Button variant="contained" color="primary" component="span">
+        <Button
+          variant="contained"
+          color="primary"
+          component="span"
+          startIcon={<CloudUploadIcon />}
+        >
           Upload
         </Button>
       </label>
